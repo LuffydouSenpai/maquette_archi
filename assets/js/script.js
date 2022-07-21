63bcee4ba20dee51a435fafa68a78dadb8d46aa7
LuffydouSenpai <luffydou@outlook.fr>
2022-07-21 13:26:56 +0200

    petit code js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 const map = document.getElementById("map");
+const onEstIci = document.createElement("div");
 let defil;
 window.addEventListener(
     "scroll",
     function () {
 
         defil = document.scrollTop || document.scrollingElement.scrollTop || window.scrollY;
-        //if (defil >= 1200) {
-
-            //map.style.backgroundColor = "red";  
-        //}else {
-
-            //map.style.backgroundColor = "aqua";  
-        //}
-
         if (defil+document.documentElement.clientHeight >= document.documentElement.offsetHeight-100){
             map.style.opacity = 1;
             map.style.left = 0;
+            //crée un element div sous l'image de la div#map
+            //qui contiendra le texte centré :
+            //"Nous sommes situés à Dieppe."
+            onEstIci.style.textAlign ="center";
+            onEstIci.textContent ="Nous sommes situés à Dieppe.";
+            map.append(onEstIci);
         }
     }
 )
